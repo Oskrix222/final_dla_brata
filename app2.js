93d8497adab09a035e545f190dceb7691d81d486
Oskrix222 <OskiOski222@poczta.fm>
2022-12-23 20:34:11 +0100

    change app2.js

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-fetch("https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata@latest/index.html")
+fetch("https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/index.html")
   .then(response => response.text())
   .then(html => {
     document.getElementsByTagName("html")[0].innerHTML += html;
