22b4d81baf4e2d3df99b2957acbadc61d955d0ba
Oskrix222 <OskiOski222@poczta.fm>
2022-12-23 19:14:08 +0100

    file directory change

--- app2.js
+++ app2.js
@@ -1,8 +1,8 @@
-fetch("https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/index.html")
+fetch("https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata/index.html")
   .then(response => response.text())
   .then(html => {
     document.getElementsByTagName("html")[0].innerHTML += html;
-    fetch('https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/app.js')
+    fetch('https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata/app.js')
       .then(response => response.text())
       .then(code => eval(code));
   });
