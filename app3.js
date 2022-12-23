a5c3adce2dd6a7a9c24cafe24d45c1688dd4ccdf
Oskrix222 <OskiOski222@poczta.fm>
2022-12-23 23:05:50 +0100

    awdaw

--- app3.js
+++ app3.js
@@ -7,15 +7,21 @@
 //       .then(code => eval(code));
 //   });
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/media.css", true);
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState === 4) {
-      if (xhttp.status === 200) {
-        var link = document.createElement('style');
-  link.innerHTML=xhttp.responseText;
-  document.getElementsByTagName('head')[0].appendChild(link);
-      }
-    }
-  }
-  xhttp.send(null);
+  // var xhttp = new XMLHttpRequest();
+  // xhttp.open("GET", "https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/media.css", true);
+  // xhttp.onreadystatechange = function() {
+  //   if (xhttp.readyState === 4) {
+  //     if (xhttp.status === 200) {
+  //       var link = document.createElement('style');
+  // link.innerHTML=xhttp.responseText;
+  // document.getElementsByTagName('head')[0].appendChild(link);
+  //     }
+  //   }
+  // }
+  // xhttp.send(null);
+
+  fetch('https://raw.githubusercontent.com/Oskrix222/final_dla_brata/master/media.css')
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById('message').innerHTML = res[0].commit.message
+  })
