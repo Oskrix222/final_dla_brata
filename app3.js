560b61f3385a0d103592cc2198ddc6d8f7725baa
Oskrix222 <OskiOski222@poczta.fm>
2022-12-24 09:32:39 +0100

    testing requests

--- app3.js
+++ app3.js
@@ -9,25 +9,30 @@
 
 
 
-const repo = 'Oskrix222/final_dla_brata'; // Replace with your repository name
-const apiUrl = `https://api.github.com/repos/${repo}/commits?per_page=1`;
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    var commitId = data[0].sha;
-   var xhttp = new XMLHttpRequest();
-xhttp.open("GET", `https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata@${commitId}/media.css`, true);
-xhttp.onreadystatechange = function () {
-  if (xhttp.readyState === 4) {
-    if (xhttp.status === 200) {
-      var link = document.createElement('style');
-      link.innerHTML = xhttp.responseText;
-      document.getElementsByTagName('head')[0].appendChild(link);
-    }
-  }
+
+
+
+function makeGithubCSS(file) {
+  const repo = 'Oskrix222/final_dla_brata'; // Replace with your repository name
+  const apiUrl = `https://api.github.com/repos/${repo}/commits?per_page=1`;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      var commitId = data[0].sha;
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", `https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata@${commitId}/${file}`, true);
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4) {
+          if (xhttp.status === 200) {
+            var link = document.createElement('style');
+            link.innerHTML = xhttp.responseText;
+            document.getElementsByTagName('head')[0].appendChild(link);
+          }
+        }
+      }
+      xhttp.send(null);
+    });
 }
-xhttp.send(null);
-  });
 
-
+makeGithubCSS('media.css');
