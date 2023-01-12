b8a606754f60c57eb38fd8b8b883a31c42a1de9f
Oskrix222 <OskiOski222@poczta.fm>
2023-01-12 18:03:28 +0100

    index changes

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
-function getLastCommitId() {
+// function getLastCommitId() {
 
 
-    fetch('https://api.github.com/repos/Oskrix222/final_dla_brata/commits')
-        .then(response => response.json())
-        .then(data => {
-            let lastCommit = data[0].sha;
-            console.log(lastCommit);
+//     fetch('https://api.github.com/repos/Oskrix222/final_dla_brata/commits')
+//         .then(response => response.json())
+//         .then(data => {
+//             let lastCommit = data[0].sha;
+//             console.log(lastCommit);
 
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("GET", `https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata@${lastCommit}/app.js`, true);
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState === 4) {
-                    if (xhttp.status === 200) {
-                        eval(xhttp.responseText);
-                    }
-                }
-            }
-        })
-}
+//             var xhttp = new XMLHttpRequest();
+//             xhttp.open("GET", `https://cdn.jsdelivr.net/gh/Oskrix222/final_dla_brata@${lastCommit}/app.js`, true);
+//             xhttp.onreadystatechange = function () {
+//                 if (xhttp.readyState === 4) {
+//                     if (xhttp.status === 200) {
+//                         eval(xhttp.responseText);
+//                     }
+//                 }
+//             }
+//         })
+// }
 
 
 
-getLastCommitId();
+// getLastCommitId();
+
+// console.log(123);
